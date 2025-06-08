0c96644124ff0ed48621a795728d97cfefa7f124
Priyanshu Verma <officialpriyanshu2003@gmail.com>
2025-06-08 20:58:23 +0530

    Create DeleteElementfromhead.java

---
+++ DeleteElementfromhead.java
@@ -0,0 +1,36 @@
+class Node{
+  public int data;
+  public Node next;
+  public Node(int data1, Node next1){
+    this.data = data1;
+    this.next = next1;
+  }
+  public Node(int data1){
+    data = data1;
+    next = null;
+  }
+}
+class Main{
+  private static Node removeHead(Node head){
+   if(head == null) return head;
+    head = head.next;
+    return head;
+  }
+  private static void printLL(Node head){
+    while(head != null){
+      System.out.print(head.data + " ");
+      head = head.next;
+    }
+  }
+  public static void main(String[] args){
+    List<Integer> arr = Arrays.asList(12,3,4,5);
+    Node head = new Node(arr.get(0));
+    head.next = new Node(arr.get(1));
+    head.next.next = new Node(arr.get(2));
+    head.next.next.next = new Node(arr.get(3));
+    System.out.println(arr);
+    head = removeHead(head);
+    
+    printLL(head);
+  }
+}
