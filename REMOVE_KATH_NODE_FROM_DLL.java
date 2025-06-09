a555042c350370a8d682e95587ff069fa0f60072
Priyanshu Verma <officialpriyanshu2003@gmail.com>
2025-06-09 16:59:05 +0530

    Create REMOVE_KATH_NODE_FROM_DLL.java

---
+++ REMOVE_KATH_NODE_FROM_DLL.java
@@ -0,0 +1,83 @@
+class Node{
+  public int data;
+  public Node next;
+  public Node back;
+  
+  public Node(int data1, Node next1, Node back1){
+    this.data = data1;
+    this.next = next1;
+    this.back = back1;
+  }
+  
+  public Node(int data1){
+    this.data = data1;
+    this.next = null;
+    this.back = null;
+  }
+}
+
+class Main{
+  public static Node removeKthNode(Node head , int k){
+    if(head == null || head.next == null) return null;
+    int count = 0;
+    Node temp = head;
+    Node prev = null;
+    while(temp != null){
+      count++;
+      if(count == k){
+        if(temp.next == null){
+          prev.next = null;
+          temp.back =null;
+          return head;
+        } else if(temp.back == null){
+          head = head.next;
+          head.back = null;
+          temp.next = null;
+          temp.back = null;
+          return head;
+        } else {
+          prev.next = temp.next;
+          temp.next.back = prev;
+       temp.next = null;
+temp.back = null;
+
+          return head;
+        }
+      }
+      prev = temp;
+      temp = temp.next;
+    }
+
+    return head;
+  }
+  public static void printLL(Node head){
+    int cnt = 0;
+    while(head != null){
+      System.out.print(head.data + " ");
+      // System.out.println("[" + head.back+ "  " + head.data + "  "+ head.next+ "]");
+      head = head.next;
+      cnt ++;
+      
+    }
+    System.out.println();
+    System.out.println(" Length of a Linkedin List is :" + cnt);
+  }
+  
+  public static Node changeToLL(int[] arr){
+    Node head = new Node(arr[0], null,null);
+    Node prev = head;
+    for(int i =1; i<arr.length; i++){
+      Node temp = new Node(arr[i],null,prev);
+      prev.next = temp;
+      prev = temp;
+    }
+    return head;
+  }
+  public static void main(String[] args){
+    int[] arr = {3,4,5,6,7,8,9,9,0};
+    System.out.println(Arrays.toString(arr));
+    Node head = changeToLL(arr);
+   
+    printLL( removeKthNode(head, 5));
+  }
+} 
